80c46c86e7de7cd8a491554e84fd9aba65a501c7
Ubuntu <ubuntu@ip-172-31-26-198.eu-west-2.compute.internal>
2025-03-27 15:34:49 +0000

    Fix Flask app and NGINX configuration

--- Task1/app.py
+++ Task1/app.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-name = os.environ.get('YOUR_NAME', 'friend')
 
 @app.route('/')
 def hello():
+    name = os.environ.get('YOUR_NAME', 'friend')
     return f"Hello, {name}!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+    app.run(host='0.0.0.0', port=5500, debug=True)
